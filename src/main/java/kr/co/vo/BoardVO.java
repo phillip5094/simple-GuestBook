1ff0e0b28089a055df19783ffe13209cc03873a4
Philip <phillip0726@gmail.com>
2019-12-18 16:02:21 +0900

    VO(Value Object) 클래스 생성 getter, setter 구현

---
+++ src/main/java/kr/co/vo/BoardVO.java
@@ -0,0 +1,26 @@
+package kr.co.vo;
+
+public class BoardVO {
+	private String email;
+	private String password;
+	private String content;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
