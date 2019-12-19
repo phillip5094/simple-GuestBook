6a330182bbd99226e4c403996e8e67d1854fedb1
Philip <phillip0726@gmail.com>
2019-12-19 16:16:53 +0900

    방명록 list service 구현

--- src/main/java/kr/co/service/BoardService.java
+++ src/main/java/kr/co/service/BoardService.java
@@ -1,7 +1,11 @@
 package kr.co.service;
 
+import java.util.List;
+
 import kr.co.vo.BoardVO;
 
 public interface BoardService {
 	public void write(BoardVO boardVO) throws Exception;
+	
+	public List<BoardVO> list() throws Exception;
 }
--- src/main/java/kr/co/service/BoardServiceImpl.java
+++ src/main/java/kr/co/service/BoardServiceImpl.java
@@ -1,5 +1,7 @@
 package kr.co.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
@@ -16,6 +18,11 @@ public class BoardServiceImpl implements BoardService{
 	@Override
 	public void write(BoardVO boardVO) throws Exception {
 		dao.write(boardVO);
+	}
+
+	@Override
+	public List<BoardVO> list() throws Exception {
+		return dao.list();
 	}
 
 }
