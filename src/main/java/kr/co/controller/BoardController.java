bceb81e88c0ef0a020f7e860220cc50fb8ba56a8
Philip <phillip0726@gmail.com>
2019-12-19 16:17:27 +0900

    방명록 list controller 등록

--- src/main/java/kr/co/controller/BoardController.java
+++ src/main/java/kr/co/controller/BoardController.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -30,7 +31,15 @@ public class BoardController {
 		
 		service.write(boardVO);
 		
-		return "redirect:/";
+		return "redirect:/board/list";
 	}
 	
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public String list(Model model) throws Exception{
+		logger.info("list");
+		
+		model.addAttribute("list", service.list());
+		
+		return "board/list";
+	}
 }
