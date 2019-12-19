818f8959958d91bf531937244daac335f66105e1
Philip <phillip0726@gmail.com>
2019-12-19 16:16:33 +0900

    list DAO 구현

--- src/main/java/kr/co/dao/BoardDAO.java
+++ src/main/java/kr/co/dao/BoardDAO.java
@@ -1,7 +1,11 @@
 package kr.co.dao;
 
+import java.util.List;
+
 import kr.co.vo.BoardVO;
 
 public interface BoardDAO {
 	public void write(BoardVO boardVO) throws Exception;
+	
+	public List<BoardVO> list() throws Exception;
 }
--- src/main/java/kr/co/dao/BoardDAOImpl.java
+++ src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -1,5 +1,7 @@
 package kr.co.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -17,4 +19,11 @@ public class BoardDAOImpl implements BoardDAO{
 	public void write(BoardVO boardVO) throws Exception{
 		sqlSession.insert("boardMapper.insert", boardVO);
 	}
+
+	@Override
+	public List<BoardVO> list() throws Exception {
+		return sqlSession.selectList("boardMapper.list");
+	}
+	
+	
 }
